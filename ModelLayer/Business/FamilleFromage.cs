1d92be217227d76920d91f9a18e5a90791b0f040
HUGO <hugofeige@hotmail.fr>
2020-11-20 11:58:53 +0100

    création famille fromage

---
+++ ModelLayer/Business/FamilleFromage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.Business
+{
+    class FamilleFromage
+    {
+        private int _id;
+        private string _nom;
+        private List<Fromage> _uneListeFromage ;
+
+        public FamilleFromage()
+        {
+            _id = 0;
+            _nom = "";
+            _uneListeFromage = new List<Fromage>();
+        }
+    }
+}
